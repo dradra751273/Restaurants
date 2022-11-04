9c9ccb475e2d59e55eec2d736aba4ec15ffcc5f6
dradra751273 <dradra751273@gmail.com>
2022-11-05 07:32:58 +0800

    feat: Add restSeeder.js and create restaurant documents to the restaurants collection in mongoDB.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://fushaowei:water1988@cluster0.plcfxth.mongodb.net/todo-list?retryWrites=true&w=majority'
+const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://fushaowei:water1988@cluster0.plcfxth.mongodb.net/restaurants?retryWrites=true&w=majority'
 mongoose.connect(DB_URI)
 const db = mongoose.connection
 db.on('error', () => {
