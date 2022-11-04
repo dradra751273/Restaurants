b9545ab116d195b100f363b704e1e1099f183f2e
dradra751273 <dradra751273@gmail.com>
2022-11-05 07:18:31 +0800

    feat: 建立 restModel 並放置各餐廳原始資料 (restSeeder.json)

---
+++ models/restModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restSchema = new Schema({
+  id: {
+    type: Number,
+    require: [true, 'A restaurant must have an id']
+  },
+  name: {
+    type: String,
+    require: [true, 'A restaurant must have a name'],
+  },
+  name_en: String,
+  category: String,
+  image: String,
+  location: {
+    type: String,
+    require: [true, 'A restaurant must have an location'],
+  },
+  phone: {
+    type: String,
+    require: [true, 'A restaurant must have a phone number'],
+  },
+  google_map: String,
+  rating: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+  },
+  description: String
+})
+
+
+module.exports = mongoose.model('Restaurants', restSchema)
