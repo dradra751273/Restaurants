6e550861d906694f81417c5f9ffaf3d97fc4c82e
dradra751273 <dradra751273@gmail.com>
2022-11-05 07:20:13 +0800

    feat: 建立 mongoose.js 管理 mongoDB 連線

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://fushaowei:water1988@cluster0.plcfxth.mongodb.net/todo-list?retryWrites=true&w=majority'
+mongoose.connect(DB_URI)
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+module.exports = db
