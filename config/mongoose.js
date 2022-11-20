4eae5b2dcc01c4be22d8bf8409d91567815b7125
dradra751273 <dradra751273@gmail.com>
2022-11-20 10:06:05 +0800

    fix: delete MongoDB password

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-const DB_URI = process.env.MONGODB_URI || 'mongodb+srv://fushaowei:water1988@cluster0.plcfxth.mongodb.net/restaurants?retryWrites=true&w=majority'
+const password = ''
+const DB_URI = process.env.MONGODB_URI || `mongodb+srv://fushaowei:${password}@cluster0.plcfxth.mongodb.net/restaurants?retryWrites=true&w=majority`
+
 mongoose.connect(DB_URI)
 const db = mongoose.connection
 db.on('error', () => {
